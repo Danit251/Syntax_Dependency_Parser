b7f65c241d80ff12d4e868fca358c6a4a155baf8
vardit <vardit@cs.huji.ac.il>
2017-12-15 11:54:04 +0200

    Parts b & d are complete

--- ex3.py
+++ ex3.py
@@ -11,7 +11,21 @@ test_set = corpus_sentences[training_size:]
 NUM_ITER = 2
 
 
+# Helper functions
+
 # for word - enter 0, for tag- enter 1
+def create_sentence(tree):
+    sentence = []
+
+    # for ROOT
+    sentence.append(("ROOT", "ROOT"))
+
+    for i in range(1, len(tree.nodes)):
+        sentence.append((tree.nodes[i]["word"], tree.nodes[i]["tag"]))
+
+    return np.array(sentence)
+
+
 def find_in_sentence(sentence, value, word_or_tag=0):
     for i, node in enumerate(sentence):
         if node[word_or_tag] == value:
@@ -19,22 +33,42 @@ def find_in_sentence(sentence, value, word_or_tag=0):
     return -1
 
 
+
 # part b
 def feature_function(node1, node2, sentence):
     length = len(sentence)
 
     word1_ind = find_in_sentence(sentence, node1[0])
-    word2_ind = find_in_sentence(sentence, node2[0]) + word1_ind * length
+    word2_ind = find_in_sentence(sentence, node2[0])
     tag1_ind = find_in_sentence(sentence, node1[1], word_or_tag=1)
-    tag2_ind = find_in_sentence(sentence, node2[1], word_or_tag=1) \
-               + length ** 2 + tag1_ind * length
+    tag2_ind = find_in_sentence(sentence, node2[1], word_or_tag=1)
 
-    # 1 for root, marked as (None, 'TOP') todo fix later
-    # additional 4 for part d, todo add later
     feature_vec = np.zeros(length ** 2 + length ** 2 + 4)
-    feature_vec[word2_ind] = 1
-    feature_vec[tag2_ind] = 1
-    pass
+    word_feature_ind = tag_feature_ind = -1
+
+    if word2_ind != -1 and word1_ind != -1:
+        word_feature_ind = word1_ind * length + word2_ind
+        feature_vec[word_feature_ind] = 1
+
+    if tag1_ind != -1 and tag2_ind != -1:
+        tag_feature_ind = length ** 2 + tag1_ind * length + tag2_ind
+        feature_vec[tag_feature_ind] = 1
+
+    # part d:
+    if word_feature_ind != -1:
+        if word2_ind-word1_ind == 1:
+            feature_vec[-1] = 1
+        else:
+            if word1_ind-word2_ind ==1:
+                feature_vec[-2] = 1
+    if tag_feature_ind != -1:
+        if tag2_ind - tag1_ind == 1:
+            feature_vec[-3] = 1
+        else:
+            if tag1_ind - tag2_ind == 1:
+                feature_vec[-4] = 1
+
+    return feature_vec
 
 
 def perceptron(feature_size, num_iter, feature_func):
@@ -62,15 +96,10 @@ def calc_tree_features(tree, sentence):
     pass
 
 
-def create_sentence(tree):
-    sentence = []
-    for i in range(len(tree.nodes)):
-        sentence.append((tree.nodes[i]["word"], tree.nodes[i]["tag"]))
-    return np.array(sentence)
+def main():
+    sentence = create_sentence(training_set[1])
+    feature_vec = feature_function(sentence[0], sentence[1], sentence)
 
 
-sentence = create_sentence(training_set[1])
-feature_function(sentence[0], sentence[1], sentence)
-# print(sentence[0][1])
-# print(sentence)
-# print(set(sentence[:,0]))
+if __name__ == "__main__":
+    main()
